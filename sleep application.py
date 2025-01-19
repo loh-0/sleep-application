4257ece016d6be0ca3046db9dba5eb1cc3c3f7a3
lkar0013 <141287447+loh-0@users.noreply.github.com>
2025-01-19 23:59:48 +1100

    Added functionality + adjusted existing code
    Changes:
    - Adjusted how the data was stored to use pandas. Using a df made storing and reading data very simply
    - Added a modelling feature which creates a linear regression model
    - Added functionality to read in csv files, and save them as well
    
    Pending Tasks:
    - Validate Input / Bug test
    - Data Visualisation Function

--- sleep application.py
+++ sleep application.py
@@ -1,13 +1,25 @@
 from datetime import datetime
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+import pandas as pd 
 
-data = []
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+
+# We create a blank df for users who wish to build their data up from scratch using this application
+
+columns = ["Date", "Hours", "Quality", "Exercise", "Caffeine", "Light_Sleep", "Deep_Sleep", "REM_Sleep"]
+
+sleep_df =  pd.DataFrame(columns=columns)
 
 def log_sleep():
 
     """
 
     Function to log the users sleep. The function requests that the user enter the data of their sleep, the hours they slept, the quality of their sleep,
-    whether or not they exercises and had caffeine and the respective hours spent in each sleep cycle.
+    whether or not they exercises and had caffeine and the respective hours spent in each sleep cycle. Using the pandas library we store the data in a df 
+    to easily perform our linear regression later
 
     """
 
@@ -24,19 +36,9 @@ def log_sleep():
     deep_sleep = float(input("Deep sleep hours: "))
     rem_sleep = float(input("REM sleep hours: "))
 
-    entry = {
-    "date": date,
-    "hours": hours,
-    "quality": quality,
-    "exercise": exercise,
-    "caffeine": caffeine,
-    "stages": {
-        "light": light_sleep,
-        "deep": deep_sleep,
-        "rem": rem_sleep
-        }
-    }
-    data.append(entry)
+
+    # entries stored as an entry in the sleep df
+    sleep_df.loc[len(sleep_df)] = [date, hours, quality, exercise, caffeine, light_sleep, deep_sleep, rem_sleep]
     
     print("Sleep data logged!")
 
@@ -45,20 +47,74 @@ def view_sleep():
 
     """
     Function to display the recorded sleep data of a user
+    """
+
+    print("Your Sleep Data:\n") #using the df we don't have to do any formatting can simply print it to the terminal 
+    print(sleep_df)
+
+
+def model_sleep():
+    """
+
+    Function that uses linear regression to model the collected sleep data. Linear Regression attempts to establish a linear relationship between the data.
+    For this, we have selected Sleep Quality to be what we are attempting to predict. We drop Date, Hours (total) since they are irrelevant and Quality for our
+    predictors. We use feature scaling as standard good practice for a linear regression model to ensure that no feature dominanates another due to it's large unit of measurement
+    and affect our results. To handle our categorical variables, we convert them to 0 and 1 and simplfy include or don't include them. Lastly, we display all our coefficients
+    at the end for the user to see.
+
+    """
+    if len(sleep_df) < 3: # not enough data to produce meaningful results 
+        print("Insufficient data to model")
+        return ""
+
+    # Since exercise and caffeine are not quantitative predictors, we can convert them into categorical predictors by including them if true (1) and 
+    # excluding if they aren't (0)
+    sleep_df["Exercise"] = sleep_df["Exercise"].apply(lambda x: 1 if x else 0)
+    sleep_df["Caffeine"] = sleep_df["Caffeine"].apply(lambda x: 1 if x else 0)
+
+
+    # We define our predictors and what we are trying to predict
+    X = sleep_df.drop(columns=['Date', 'Hours', 'Quality'])
+    y = sleep_df['Quality']
+
+    # Feature scaling is using to prevent large scale features to dominant other features 
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    # Create our model
+    linear_model = LinearRegression().fit(X_scaled, y)
+
+    print(linear_model.coef_)
+
+
+def read_data():
+    
+    """
+    The function is responsible for reading the data into the df. Uses Tkinter to display a pop up to make this process idea. We use the basic .read_csv() method enter the data easily.
+    
+    """
+        
+    global sleep_df # required to access the global variable sleep_df, otherwise it will create a local variable
+    print("Warning! Read Data will Override any existing data you have entered.")
+    # UsingTkinter we can open a file dialog to allow user to select their csv file
+    file_name = askopenfilename(title="Select a CSV File",filetypes=[("CSV Files", "*.csv"), ("All Files", "*.*")])
+    if not file_name:
+        print("No file selected.")
+        return 
+    # Read the selected CSV file into a df
+    sleep_df = pd.read_csv(file_name)
+
+
+def save_data():
+    """
+    Function for saving data as a csv. Prompts the user to select a file name. Adds ".csv" at the end so that it will be correctly displayed for the read_data() function. 
     
     """
 
-    if len(data) == 0: 
-        print("There is no sleep data to view.")
-        return
-
-    print("Your Sleep Data:\n")
-
-    for i, entry in enumerate(data): # accessses the stored sleep information and displays 
-        print(f"{i+1}. Date: {entry['date']}, Hours: {entry['hours']}, Quality: {entry['quality']}")
-        print(f"Exercise: {entry['exercise']}, Caffeine: {entry['caffeine']}")
-        print(f"Sleep Stages - Light: {entry['stages']['light']}, Deep: {entry['stages']['deep']}, REM: {entry['stages']['rem']}")
-
+    file_name = input("Enter File Name: ")
+    file_name = file_name + ".csv" # adds .csv to end of any file name so it's formatted correctly
+    sleep_df.to_csv(file_name,index=False) # index set to false as we don't need indexes 
+    print(f"Data successfully saved to {file_name}")
 
 def main():
 
@@ -72,7 +128,11 @@ def main():
         print("\nSleep Tracker")
         print("1. Log Sleep")
         print("2. View Data")
-        print("3. Exit")
+        print("3. Save Data")
+        print("4. Read Data")
+        print("5. Model Data")
+        print("6. Visualise Data")
+        print("7. Exit")
         
         
         option = input("Choose an option: ")
@@ -81,7 +141,18 @@ def main():
         elif option == "2":
             view_sleep()
         elif option == "3":
-            print("Goodbye!")
+            save_data()
+        elif option == "4":
+            read_data()
+    
+        elif option == "5":
+            model_sleep()
+
+        # elif option == "6":
+        #     visualise_sleep()
+     
+        elif option == "7":
+            print("Closing...")
             break
         else:
             print("Invalid Selection.")
